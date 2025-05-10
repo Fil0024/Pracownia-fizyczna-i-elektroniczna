1064dabd4f154cd1443bbc1667ff6ce039b0dbed
Filip Gołębiewski <140067370+Fil0024@users.noreply.github.com>
2025-05-10 16:03:13 +0200

    Add files via upload

--- analysis/calculations.py
+++ analysis/calculations.py
@@ -1,15 +1,13 @@
 import numpy as np
+from sympy import symbols, diff, solve, sqrt, N
 from scipy.optimize import curve_fit
 from data_loader import load_data
 
-def f_to_omega(f):
-    """Hz -> rad/s"""
-    return 2 * np.pi * f
-
-def calc_transmission(df, U1='U_in', U2='U_out'):
+def calc_transmission(df):
     #Dodaje do df kolumnę T = Uwy/Uwe (transmitancję)
     df = df.copy()
-    df['T'] = df[U2] / df[U1]
+    df['T'] = df['U_out'] / df['U_in']
+    df['u_T'] = df['T'] * np.sqrt((df['u_Uout'] / df['U_out'])**2 + (df['u_Uin'] / df['U_in'])**2)
     return df
 
 def T_function(omega, R, L, C, Rp):
@@ -24,9 +22,11 @@ def phi_function(omega, R, L, C, Rp):
     """
     Funkcja przesunięcia fazowego
     """
-    return np.arctan((1 - omega**2 * L * C) / (omega*(R+Rp)*C))
+    num = 1 - omega**2 * L * C
+    den = omega*(R+Rp)*C
+    return np.arctan2(num, den)
 
-def fit_T(omega, T_exp, R, C, p0=(1e-3, 10)):
+def fit_T(omega, T_exp, u_T_exp, R, C, p0=(1e-3, 20)):
     """
     Dopasowanie L i Rp do transmitancji.
     """
@@ -35,10 +35,16 @@ def fit_T(omega, T_exp, R, C, p0=(1e-3, 10)):
 
     popt, pcov = curve_fit(model, omega, T_exp, p0=p0)
     perr = np.sqrt(np.diag(pcov))
-    return popt, perr
+
+    residuals = (T_exp - model(omega, *popt)) / u_T_exp
+    chi2 = np.sum(residuals**2)
+    dof = len(omega) - len(popt)   # N - p
+    chi2_red = chi2 / dof
+
+    return popt, perr, chi2_red
 
 
-def fit_phi(omega, phi_exp, R, C, p0=(1e-3, 10)):
+def fit_phi(omega, phi_exp, u_phi_exp, R, C, p0=(1e-3, 30)):
     """
     Dopasowanie L i Rp do przesunięcia fazowego.
     """
@@ -47,25 +53,70 @@ def fit_phi(omega, phi_exp, R, C, p0=(1e-3, 10)):
 
     popt, pcov = curve_fit(model, omega, phi_exp, p0=p0)
     perr = np.sqrt(np.diag(pcov))
-    return popt, perr
+
+    residuals = (phi_exp - model(omega, *popt)) / u_phi_exp
+    chi2 = np.sum(residuals**2)
+    dof = len(omega) - len(popt)
+    chi2_red = chi2 / dof
+
+    return popt, perr, chi2_red
 
 def calc_parameters(df, R, C):
-    df = calc_transmission(df, U1='U_in', U2='U_out')
+    df = calc_transmission(df)
+    omega = df['omega'].to_numpy()
 
-    omega   = f_to_omega(df['Freq'].to_numpy())
-    T_exp   = df['T'].to_numpy()
-    phi_exp = np.deg2rad(df['Phi'].to_numpy())
+    T_exp, u_T_exp   = df['T'].to_numpy(), df['u_T'].to_numpy()
+    phi_exp, u_phi_exp = np.pi*df['Phi'].to_numpy()/180, df['u_Phi'].to_numpy()*np.pi/180
 
-    (L_T, Rp_T),  (u_LT, u_RpT)  = fit_T(omega, T_exp,   R, C)
-    (L_P, Rp_P),  (u_LP, u_RpP)  = fit_phi(omega, phi_exp, R, C)
+    (L_T, Rp_T),  (u_LT, u_RpT), chi2_red_T  = fit_T(omega, T_exp, u_T_exp,   R, C)
+    (L_P, Rp_P),  (u_LP, u_RpP), chi2_red_P  = fit_phi(omega, phi_exp, u_phi_exp, R, C)
 
     return {
         "L_T": L_T,
         "Rp_T": Rp_T,
         "u_LT": u_LT,
         "u_RpT": u_RpT,
+        "chi2_red_T": chi2_red_T,
         "L_P": L_P,
         "Rp_P": Rp_P,
         "u_LP": u_LP,
-        "u_RpP": u_RpP
+        "u_RpP": u_RpP,
+        "chi2_red_P": chi2_red_P
     }
+    
+
+def calc_resonance_and_bandwidth(R, L, C, Rp):
+    x = symbols('x', real=True, positive=True)
+
+    T = R / sqrt((R + Rp)**2 + (x*L - 1/(x*C))**2)
+
+    dT = diff(T, x)
+    crit_pts = solve(dT, x)
+
+    crit_real = [pt for pt in crit_pts if pt.is_real and pt > 0]
+
+    crit_vals = [float(N(T.subs(x, pt))) for pt in crit_real]
+
+    idx_max = crit_vals.index(max(crit_vals))
+    x_res = float(N(crit_real[idx_max]))        
+    T_max = crit_vals[idx_max]             
+
+    cutoff_level = T_max / (2**0.5)
+    eq = T - cutoff_level
+    cut_sols = solve(eq, x)
+
+    cut_real = [sol for sol in cut_sols if sol.is_real and sol > 0]
+
+    cut_vals = sorted([float(N(sol)) for sol in cut_real])
+
+    if len(cut_vals) >= 2:
+        bandwidth = cut_vals[-1] - cut_vals[0]
+    else:
+        bandwidth = None
+        
+    theoretical_x_res = 1 / (sqrt(L * C))
+
+    theoretical_Rp = 1/T_max * ( R - T_max * R)
+
+    return x_res, bandwidth, theoretical_x_res, T_max, theoretical_Rp
+    
--- analysis/main.py
+++ analysis/main.py
@@ -8,33 +8,44 @@ path_in2 = 'dane/pkt5.csv'
 path_figures = 'raport/figures'
 path_results = 'analiza/results.txt'
 
-R1 = 510        # Ohm
-R2 = 50        # Ohm
+R1 = 506        # Ohm
+R2 = 50.2        # Ohm
 C = 1e-9      # F
 
-data1 = load_data(path_in1)
-data2 = load_data(path_in2)
+def show_results(data,  R, C, name='no_name'):
+    data['omega'] = 2 * np.pi * data['Freq']
+    data['u_Uin'] = data['U_in'] * 0.03 / np.sqrt(3)
+    data['u_Uout'] = data['U_out'] * 0.03 / np.sqrt(3)
+    data['u_Phi'] = data['Phi'] * 0.03 / np.sqrt(3)
+    results = calc.calc_parameters(data, R, C)
+    exp_resonance1, exp_bandwidth1, theoretical_resonance1, exp_transmitance_rez, Theoretical_Rp  = calc.calc_resonance_and_bandwidth(R1, results['L_T'], C, results['Rp_T'])
 
-# Calculate parameters
-results1 = calc.calc_parameters(data1, R1, C)
-results2 = calc.calc_parameters(data2, R2, C)
+    with open(path_results, 'a') as f:
+        f.write("Results for "+name+":\n")
+        f.write(f"L_T = {results['L_T']:.6f} +- {results['u_LT']:.6f} H\n")
+        f.write(f"Rp_T = {results['Rp_T']:.4f} +- {results['u_RpT']:.4f} Ohm\n")
+        f.write(f"L_P = {results['L_P']:.6f} +- {results['u_LP']:.6f} H\n")
+        f.write(f"Rp_P = {results['Rp_P']:.4f} +- {results['u_RpP']:.4f} Ohm\n")
+        f.write(f"chi2_red_T = {results['chi2_red_T']:.4f}\n")
+        f.write(f"chi2_red_P = {results['chi2_red_P']:.4f}\n")
+        f.write(f"Experimental resonance frequency (omega0) = {exp_resonance1:.4f} 1/s \n")
+        f.write(f"Experimental resonance transmitance (Trez) = {exp_transmitance_rez:.4f} \n")
+        f.write(f"Theoretical Rp = {Theoretical_Rp:.4f} Ohm \n")
+        f.write(f"Theoretical resonance frequency (omega0) = {theoretical_resonance1:.4f} 1/s \n")
+        f.write(f"Experimental bandwidth (delta_omega) = {exp_bandwidth1:.4f} 1/s \n\n")
+
+    visualise_data(data, results, R, C, path_figures, name=name)
 
 
-# Save results to a text file
-with open(path_results, 'w') as f:
-    f.write("Results for pkt4.csv:\n")
-    f.write(f"L_T = {results1['L_T']:.4f} +- {results1['u_LT']:.4f}\n")
-    f.write(f"Rp_T = {results1['Rp_T']:.4f} +- {results1['u_RpT']:.4f}\n")
-    f.write(f"L_P = {results1['L_P']:.4f} +- {results1['u_LP']:.4f}\n")
-    f.write(f"Rp_P = {results1['Rp_P']:.4f} +- {results1['u_RpP']:.4f}\n\n")
+if __name__ == "__main__":
+    with open(path_results, 'w') as f:
+        f.write("")
 
-    f.write("Results for pkt5.csv:\n")
-    f.write(f"L_T = {results2['L_T']:.4f} +- {results2['u_LT']:.4f}\n")
-    f.write(f"Rp_T = {results2['Rp_T']:.4f} +- {results2['u_RpT']:.4f}\n")
-    f.write(f"L_P = {results2['L_P']:.4f} +- {results2['u_LP']:.4f}\n")
-    f.write(f"Rp_P = {results2['Rp_P']:.4f} +- {results2['u_RpP']:.4f}\n")
+    data1 = load_data(path_in1)
+    data2 = load_data(path_in2)
 
-visualise_data(data1, results1, R1, C, path_figures, 'pkt4')
-visualise_data(data2, results2, R2, C, path_figures, 'pkt5')
-print("Results saved to", path_results)
-print("Figures saved to", path_figures)
+    show_results(data1, R1, C, name='pkt4')
+    show_results(data2, R2, C, name='pkt5')
+
+    print("Results saved to", path_results)
+    print("Figures saved to", path_figures)
